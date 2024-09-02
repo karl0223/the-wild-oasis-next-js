2dae8a02cb23cbeeb0b452153c53ad4825ad8018
karl0223 <karlducta022@gmail.com>
2024-09-02 14:32:13 +0800

    Creating a Layout

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -3,7 +3,6 @@ import Navigation from '../components/Navigation';
 export default function Page() {
     return (
         <div>
-            <Navigation />
             <h1>Cabins Page</h1>
         </div>
     );
