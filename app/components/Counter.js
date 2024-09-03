120454983d6f390389512a0991583c68541fd0ae
karl0223 <karlducta022@gmail.com>
2024-09-03 13:09:39 +0800

    Adding Interactivity with Client Components

---
+++ app/components/Counter.js
@@ -0,0 +1,16 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function Counter({ users }) {
+    const [count, setCount] = useState(0);
+
+    console.log(users);
+
+    return (
+        <div>
+            <p>There are {users.length} users</p>
+            <button onClick={() => setCount((c) => c + 1)}>{count}</button>
+        </div>
+    );
+}
