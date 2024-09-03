b84e80acccbcfbe105c71e9db6b17192db5ca618
karl0223 <karlducta022@gmail.com>
2024-09-03 12:57:47 +0800

    data fetching

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,9 +1,16 @@
-import Navigation from '../components/Navigation';
+export default async function Page() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
 
-export default function Page() {
+    console.log(data);
     return (
         <div>
             <h1>Cabins Page</h1>
+            <ul>
+                {data.map((user) => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
         </div>
     );
 }
