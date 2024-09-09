989d3a96ea9f53641fdcdda7cf5a37dad6dc6ac1
karl0223 <karlducta022@gmail.com>
2024-09-09 18:48:27 +0800

    Error handling not found errors

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,3 +1,4 @@
+import { notFound } from 'next/navigation';
 import { eachDayOfInterval } from 'date-fns';
 import { supabase } from './supabase';
 
@@ -16,6 +17,7 @@ export async function getCabin(id) {
 
     if (error) {
         console.error(error);
+        notFound();
     }
 
     return data;
