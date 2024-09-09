a84958b5ef8aa3cdba606fed5e2a7d87c3a7471e
karl0223 <karlducta022@gmail.com>
2024-09-09 17:21:50 +0800

    Streaming UI with Suspense Cabin Lists

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -41,6 +41,8 @@ export const getCabins = async function () {
         .select('id, name, maxCapacity, regularPrice, discount, image')
         .order('name');
 
+    // await new Promise((res) => setTimeout(res, 2000));
+
     if (error) {
         console.error(error);
         throw new Error('Cabins could not be loaded');
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,13 +1,12 @@
-import CabinCard from '@/app/_components/CabinCard';
-import { getCabins } from '../_lib/data-service';
+import CabinLists from '@/app/_components/CabinLists';
+import { Suspense } from 'react';
+import Spinner from '@/app/_components/Spinner';
 
 export const metadata = {
     title: 'Cabins',
 };
 
-export default async function Page() {
-    const cabins = await getCabins();
-
+export default function Page() {
     return (
         <div>
             <h1 className='text-4xl mb-5 text-accent-400 font-medium'>
@@ -22,13 +21,9 @@ export default async function Page() {
                 perfect spot for a peaceful, calm vacation. Welcome to paradise.
             </p>
 
-            {cabins.length > 0 && (
-                <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14'>
-                    {cabins.map((cabin) => (
-                        <CabinCard cabin={cabin} key={cabin.id} />
-                    ))}
-                </div>
-            )}
+            <Suspense fallback={<Spinner />}>
+                <CabinLists />
+            </Suspense>
         </div>
     );
 }
