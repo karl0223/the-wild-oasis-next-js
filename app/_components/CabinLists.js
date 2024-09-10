661085fcc9238127c7746b33d1e4b67eebfe34a1
karl0223 <karlducta022@gmail.com>
2024-09-10 15:09:17 +0800

    Sharing State Between Client and Server The URL

--- app/_components/CabinLists.js
+++ app/_components/CabinLists.js
@@ -2,7 +2,7 @@ import { unstable_noStore as noStore } from 'next/cache';
 import CabinCard from '@/app/_components/CabinCard';
 import { getCabins } from '../_lib/data-service';
 
-export default async function CabinLists() {
+export default async function CabinLists({ filter }) {
     // opt out cache --- does not cache any data
     // noStore();
 
@@ -10,9 +10,24 @@ export default async function CabinLists() {
 
     if (!cabins.length) return null;
 
+    let displayedCabins;
+
+    if (filter === 'all') displayedCabins = cabins;
+
+    if (filter === 'small')
+        displayedCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+
+    if (filter === 'medium')
+        displayedCabins = cabins.filter(
+            (cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7
+        );
+
+    if (filter === 'large')
+        displayedCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+
     return (
         <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14'>
-            {cabins.map((cabin) => (
+            {displayedCabins.map((cabin) => (
                 <CabinCard cabin={cabin} key={cabin.id} />
             ))}
         </div>
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,6 +1,7 @@
 import CabinLists from '@/app/_components/CabinLists';
 import { Suspense } from 'react';
 import Spinner from '@/app/_components/Spinner';
+import Filter from '@/app/_components/Filter';
 
 // revalidate cache / fetch new data ---- data cache / full route approach
 export const revalidate = 3600; // seconds
@@ -10,7 +11,8 @@ export const metadata = {
     title: 'Cabins',
 };
 
-export default function Page() {
+export default function Page({ searchParams }) {
+    const filter = searchParams?.capacity ?? 'all';
     return (
         <div>
             <h1 className='text-4xl mb-5 text-accent-400 font-medium'>
@@ -25,8 +27,12 @@ export default function Page() {
                 perfect spot for a peaceful, calm vacation. Welcome to paradise.
             </p>
 
-            <Suspense fallback={<Spinner />}>
-                <CabinLists />
+            <div className='flex justify-end mb-8'>
+                <Filter />
+            </div>
+
+            <Suspense fallback={<Spinner />} key={filter}>
+                <CabinLists filter={filter} />
             </Suspense>
         </div>
     );
