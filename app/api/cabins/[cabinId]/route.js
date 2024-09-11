9b042a1f744a4d75d558165d9ea3b28183f53840
karl0223 <karlducta022@gmail.com>
2024-09-11 10:33:23 +0800

    Creating an API Endpoint with Route Handlers

---
+++ app/api/cabins/[cabinId]/route.js
@@ -0,0 +1,20 @@
+import { getBookedDatesByCabinId, getCabin } from '@/app/_lib/data-service';
+
+export async function GET(request, { params }) {
+    const { cabinId } = params;
+
+    try {
+        const [cabin, bookedDates] = await Promise.all([
+            getCabin(cabinId),
+            getBookedDatesByCabinId(cabinId),
+        ]);
+
+        return Response.json({ cabin, bookedDates });
+    } catch {
+        return Response.json({ message: 'Cabin not found' });
+    }
+
+    return Response.json({ test: 'test' });
+}
+
+// export async function POST(request, { params }){}
