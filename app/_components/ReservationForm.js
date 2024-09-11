8b8e6dbd4e4ff91531d4fb6adcddfdd33a81b0c2
karl0223 <karlducta022@gmail.com>
2024-09-11 18:27:56 +0800

    Getting the User Session

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -1,6 +1,6 @@
 'use client';
 
-function ReservationForm({ cabin }) {
+function ReservationForm({ cabin, user }) {
     // CHANGE
     const { maxCapacity } = cabin;
 
@@ -9,16 +9,16 @@ function ReservationForm({ cabin }) {
             <div className='bg-primary-800 text-primarycabin00 px-16 py-2 flex justify-between items-center'>
                 <p>Logged in as</p>
 
-                {/* <div className='flex gap-4 items-center'>
-          <img
-            // Important to display google profile images
-            referrerPolicy='no-referrer'
-            className='h-8 rounded-full'
-            src={user.image}
-            alt={user.name}
-          />
-          <p>{user.name}</p>
-        </div> */}
+                <div className='flex gap-4 items-center'>
+                    <img
+                        // Important to display google profile images
+                        referrerPolicy='no-referrer'
+                        className='h-8 rounded-full'
+                        src={user.image}
+                        alt={user.name}
+                    />
+                    <p>{user.name}</p>
+                </div>
             </div>
 
             <form className='bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col'>
