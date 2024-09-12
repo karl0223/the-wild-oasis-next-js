3cf7e090210d7be3f4f628a6baa3bd636188fedc
karl0223 <karlducta022@gmail.com>
2024-09-12 15:08:23 +0800

    Manual Cache Revalidation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,5 +1,6 @@
 'use server';
 
+import { revalidatePath } from 'next/cache';
 import { auth, signIn, signOut } from './auth';
 import { supabase } from './supabase';
 
@@ -22,6 +23,8 @@ export async function updateGuest(formData) {
         .eq('id', session.user.guestId);
 
     if (error) throw new Error('Guest could not be updated');
+
+    revalidatePath('/account/profile');
 }
 
 export async function signInAction() {
