5b2dc2935070674a6605b0b50ac181792deff7fb
karl0223 <karlducta022@gmail.com>
2024-09-13 13:40:03 +0800

    Back to the Cabin Page Finishing the Date Selector

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,6 +1,11 @@
 'use client';
 
-import { isWithinInterval } from 'date-fns';
+import {
+    differenceInDays,
+    isPast,
+    isSameDay,
+    isWithinInterval,
+} from 'date-fns';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 import { useReservation } from './ReservationContext';
@@ -18,29 +23,36 @@ function isAlreadyBooked(range, datesArr) {
 function DateSelector({ settings, bookedDates, cabin }) {
     const { range, setRange, resetRange } = useReservation();
 
-    // CHANGE
-    const regularPrice = 23;
-    const discount = 23;
-    const numNights = 23;
-    const cabinPrice = 23;
-
+    const displayRange = isAlreadyBooked(range, bookedDates) ? {} : range;
     // SETTINGS
     const { minBookingLength, maxBookingLength } = settings;
+
+    const { regularPrice, discount } = cabin;
+    const numNights = differenceInDays(displayRange.to, displayRange.from);
+    const cabinPrice = numNights * (regularPrice - discount);
 
     return (
         <div className='flex flex-col justify-between'>
             <DayPicker
                 className='pt-12 place-self-center'
                 mode='range'
+                selected={displayRange}
+                onSelect={setRange}
                 min={minBookingLength + 1}
                 max={maxBookingLength}
-                selected={range}
-                onSelect={setRange}
-                fromMonth={new Date()}
-                fromDate={new Date()}
-                toYear={new Date().getFullYear() + 5}
+                defaultMonth={new Date()} // Starting from the current month
                 captionLayout='dropdown'
-                numberOfMonths={2}
+                numberOfMonths={2} // Show 2 months
+                // disabled={[
+                //     {
+                //         from: new Date(2024, 6, 1), // Disable bookings outside the allowed range
+                //         to: new Date(), // Disable dates before today
+                //     },
+                // ]}
+                disabled={(curDate) =>
+                    isPast(curDate) ||
+                    bookedDates.some((date) => isSameDay(date, curDate))
+                }
             />
 
             <div className='flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]'>
